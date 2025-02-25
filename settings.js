94396bfaa367883431973260c6081cb2a5a7cf25
Momen2026 <ssttyyii44@gmail.com>
2025-02-25 16:09:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'SnapLoad',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "SnapLoad Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '249913895843', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
